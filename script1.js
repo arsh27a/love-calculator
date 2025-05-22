3b73acf2428ce6aaa9e455bb837b86d71e61313f
arsh27a <arshdeepkaue08848@gmail.com>
2025-05-22 18:32:12 +0530

    Responsive design done

---
+++ script1.js
@@ -0,0 +1,18 @@
+function getRandomNumber() {
+  let randomNumber = Math.random() * 100;
+  randomNumber = Math.floor(randomNumber) + 1;
+  return randomNumber;
+}
+
+const handleClick = () => {
+  let userName = document.getElementById("userInput").value;
+  let partnerName = document.getElementById("partnerInput").value;
+  if (userName.length === 0 || partnerName.length === 0) {
+    alert("Enter your or your partner name");
+  } else {
+    let number = getRandomNumber();
+    alert(`${userName} love percentage with ${partnerName} is ${number}`);
+  }
+};
+
+document.getElementById("submitBtn").addEventListener("click", handleClick);
